343177be11ce2e7417b6a557411985be9298a79f
ariccillospark <ariccillo@sparkdigital.com>
2018-09-28 15:31:42 -0300

    NGA-4820: UI convas style (#23)
    * NGA-5975: Electron outline.
    
    * NGA-5976: Charges.
    
    * Fixed bugs reported by Nate.
    
    * UI - canvas style: Improvements.

--- src/render/2d/kekule.render.renderer2D.js
+++ src/render/2d/kekule.render.renderer2D.js
@@ -549,6 +549,21 @@ Kekule.Render.Base2DRenderer = Class.create(Kekule.Render.CompositeRenderer,  //
 	drawRichText: function(context, coord, richText, options)  // note: return {drawnObj, boundRect}
 	{
 		var drawer = this.getRichTextDrawer();
+		options.fontWeight = 'bold';
+		if (richText.text) {
+			var sign = richText.text;
+			options.strokeColor = 'transparent';
+			options.globalCompositeOperation = 'destination-over';
+			var radius = 10;
+			if (sign.charAt(0) === '-' || sign.charAt(0) === '+')
+					options.fontSize = 20;
+			if (sign.includes('+'))
+					options.fillColor = 'rgb(172,219,162)';
+			if (sign.includes('-'))
+					options.fillColor = 'rgb(255,175,175)';
+			richText.text = sign.replace('-', '\u2012');
+			this.drawCircle(context, coord, radius, options);
+		}
 		// debug
 		//console.log('draw richText', richText, options);
 		if (this.__$redirectContextDebug__)
@@ -1454,6 +1469,9 @@ Kekule.Render.UnbondedElectronSetRenderer = Class.create(Kekule.Render.ChemObj2D
 	/** @private */
 	_drawSingleElectron: function(context, coord, radius, options)
 	{
+		options.strokeColor = '#ffffff';
+		options.strokeWidth = 4;
+		options.globalCompositeOperation = 'destination-over';
 		return this.drawCircle(context, coord, radius, options);
 	},
 	/** @private */
@@ -3283,6 +3301,8 @@ Kekule.Render.ChemCtab2DRenderer = Class.create(Kekule.Render.Ctab2DRenderer,
 		if (localOptions)
 			renderOptions = Object.extend(renderOptions, localOptions);
 		*/
+
+		renderOptions.globalCompositeOperation = 'destination-over';
 
 		var c1 = this.getTransformedCoord2D(context, node1, finalTransformOptions.allowCoordBorrow);
 		var c2 = this.getTransformedCoord2D(context, node2, finalTransformOptions.allowCoordBorrow);
